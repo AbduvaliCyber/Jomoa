124cd9174a6097065cee5af36b4f604c30e1ae3c
Abduvali <abduvalierkinjonov@gmail.com>
2025-05-18 11:24:21 +0500

    first commit

--- main.cpp
+++ main.cpp
@@ -1,66 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <memory>
-#include <fstream>
+#include <map>
 using namespace std;
 
-class Plant {
-protected:
-    string name;
-    int age;       // yosh (yil)
-    float height;  // balandlik (sm)
-
-public:
-    Plant(const string& n = "Unknown", int a = 0, float h = 0.0f)
-        : name(n), age(a), height(h) {}
-
-    virtual ~Plant() {}
-
-    virtual void grow() {
-        age++;
-        height += 5.0f;  // har yili 5 sm o‘sadi
-    }
-
-    virtual void print() const {
-        cout << "Plant: " << name << ", Age: " << age << " years, Height: " << height << " cm\n";
-    }
-};
-
-class Bush : public Plant {
-private:
-    int branches; // shoxlar soni
-
-public:
-    Bush(const string& n = "Bush", int a = 0, float h = 0.0f, int b = 0)
-        : Plant(n, a, h), branches(b) {}
-
-    void grow() override {
-        Plant::grow();
-        branches += 3;  // har yili 3ta shox ko‘payadi
-    }
-
-    void print() const override {
-        cout << "Bush: " << name << ", Age: " << age << " years, Height: " << height
-             << " cm, Branches: " << branches << "\n";
-    }
-};
 
 int main() {
-    vector<shared_ptr<Plant>> garden;
+    map<string,string> data = {
+        {"Dilshoda","Gentra"},
+        {"Abduvali","Malibu3"},
+        {"Elnurbek","Coblt"},
+        {"Behruzbek","Malibu3"},
+        {"Ziyodulo","Laceti"},
+    };
 
-    garden.push_back(make_shared<Bush>("Rose", 1, 30.0f, 10));
-    garden.push_back(make_shared<Bush>("Lilac", 2, 50.0f, 15));
-
-    for (auto& plant : garden) {
-        plant->print();
-        plant->grow();
+    for (auto   d :data) {
+        cout << d.first << "s fav car is " << d.second << endl;
     }
 
-    cout << "After one year:\n";
-    for (auto& plant : garden) {
-        plant->print();
-    }
 
     return 0;
 }
